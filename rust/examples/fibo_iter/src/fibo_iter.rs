0ac1c386c424e53a6da4594296abb13c7db3f9da
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2015-11-17 02:06:00 +0800

    Update: Rust Fibonacci Iterator

--- rust/examples/fibo_iter/src/fibo_iter.rs
+++ rust/examples/fibo_iter/src/fibo_iter.rs
@@ -1,3 +1,7 @@
+extern crate num;
+
+use num::bigint::ToBigInt;
+
 fn main() {
     let fibonacci_i32_iter = (0..).scan((-1i32, 1), |state, _| {
         *state = (state.1, state.0 + state.1);
@@ -9,6 +13,11 @@ fn main() {
         Some(state.1)
     });
 
+    let fibonacci_num_iter = (0..).scan((-1.to_bigint().unwrap(), 1.to_bigint().unwrap()), |state, _| {
+        *state = (state.1.clone(), (&state.0 + &state.1).clone());
+        Some(state.1.clone())
+    });
+
     for (i, j) in fibonacci_i32_iter.take(47).enumerate() {
         println!("{:2}: {}", i, j);
     }
@@ -17,5 +26,11 @@ fn main() {
 
     for (i, j) in fibonacci_i64_iter.take(93).enumerate() {
         println!("{:2}: {}", i, j);
+    }
+
+    println!("");
+
+    for (i, j) in fibonacci_num_iter.take(200).enumerate() {
+        println!("{:3}: {}", i, j);
     }
 }
