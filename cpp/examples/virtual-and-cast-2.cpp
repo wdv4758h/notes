b23a5c8b06379dbf16cb0f06bb66dcb9799c594b
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2016-01-22 21:42:12 +0800

    Update: C++ Examples - Virtual and Cast

---
+++ cpp/examples/virtual-and-cast-2.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+template<class T>
+class Base {
+public:
+    virtual void f() {
+        std::cout << "Base    (Virtual)" << std::endl;
+    }
+
+    void g() {
+        std::cout << "Base    (Non-Virutal)" << std::endl;
+    }
+
+    void h() {
+        static_cast<T*>(this)->g();
+    }
+};
+
+class Derived : public Base<Derived> {
+public:
+    void f() {
+        std::cout << "Derived (Virtual)" << std::endl;
+    }
+
+    void g() {
+        std::cout << "Derived (Non-Virtual)" << std::endl;
+    }
+};
+
+template<class T>
+T& func(T& tmp) {
+
+    // call Derived for virtual
+    // call Base for non-virtual
+
+    tmp.f();    // virtual
+    tmp.g();    // non-virtual
+    tmp.h();    // non-virtual
+
+    return tmp;
+}
+
+int main() {
+
+    Derived foo;
+
+    std::cout << "===== Derived" << std::endl;
+
+    foo.f();    // Derived
+    foo.g();    // Derived
+    foo.h();    // Derived
+
+    std::cout << "===== Derived with static_cast<Derived::Base>" << std::endl;
+
+    static_cast<Derived::Base>(foo).f();     // Base
+    static_cast<Derived::Base>(foo).g();     // Base
+    static_cast<Derived::Base>(foo).h();     // Derived
+
+    std::cout << "===== Derived with static_cast<Derived::Base&>" << std::endl;
+
+    static_cast<Derived::Base&>(foo).f();    // Derived
+    static_cast<Derived::Base&>(foo).g();    // Base
+    static_cast<Derived::Base&>(foo).h();    // Derived
+
+    std::cout << "===== Derived with func1(T&)" << std::endl;
+
+    Derived& bar = func(foo);
+
+    std::cout << "===== Derived with T func1(T&)" << std::endl;
+
+    bar.f();    // Derived
+    bar.g();    // Derived
+    bar.h();    // Derived
+
+    return 0;
+}
