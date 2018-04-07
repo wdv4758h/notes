0a22d33751aed13464afa92276a07218ae88e847
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2018-04-08 03:46:43 +0800

    Update: GStreamer

---
+++ multimedia/examples/gstreamer/rust/0-gst-hello/src/main.rs
@@ -0,0 +1,41 @@
+extern crate gstreamer as gst;
+
+use gst::prelude::*;
+
+fn main() {
+    // Initialize GStreamer
+    gst::init().unwrap();
+
+    // Build the pipeline
+    let uri =
+        "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm";
+    let pipeline = gst::parse_launch(&format!("playbin uri={}", uri)).unwrap();
+
+    // Start playing
+    let ret = pipeline.set_state(gst::State::Playing);
+    assert_ne!(ret, gst::StateChangeReturn::Failure);
+
+    // Wait until error or EOS
+    let bus = pipeline.get_bus().unwrap();
+    while let Some(msg) = bus.timed_pop(gst::CLOCK_TIME_NONE) {
+        use gst::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {:?}: {} ({:?})",
+                    err.get_src().map(|s| s.get_path_string()),
+                    err.get_error(),
+                    err.get_debug()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
+
+    // Shutdown pipeline
+    let ret = pipeline.set_state(gst::State::Null);
+    assert_ne!(ret, gst::StateChangeReturn::Failure);
+}
